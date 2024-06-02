6b1bfee3f15d918ea08cb2afec0955e7ce163356
CJ Pye <jimmycliff4@gmail.com>
2024-06-02 15:57:05 -0500

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,30 @@
+<script>
+  import "../app.css";
+</script>
+
+<!-- Navbar -->
+
+<ul class="menu bg-base-100 w-56 rounded-box fixed left-0 h-full">
+  <li><a>Item 1</a></li>
+  <li>
+    <details open>
+      <summary>Parent</summary>
+      <ul>
+        <li><a>Submenu 1</a></li>
+        <li><a>Submenu 2</a></li>
+        <li>
+          <details open>
+            <summary>Parent</summary>
+            <ul>
+              <li><a>Submenu 1</a></li>
+              <li><a>Submenu 2</a></li>
+            </ul>
+          </details>
+        </li>
+      </ul>
+    </details>
+  </li>
+  <li><a>Item 3</a></li>
+</ul>
+
+<slot />
