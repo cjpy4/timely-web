17c862d257b7de2b001cc974db6634b48e82215d
CJ Pye <cjpye@CJs-Mac-Studio.localdomain>
2024-07-01 17:08:49 -0500

    Adding Supabase

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,22 @@
 <script>
   import "../app.css";
+	import { invalidate } from '$app/navigation'
+	import { onMount } from 'svelte'
+
+	export let data
+
+	let { supabase, session } = data
+	$: ({ supabase, session } = data)
+
+	onMount(() => {
+		const { data } = supabase.auth.onAuthStateChange((event, _session) => {
+			if (_session?.expires_at !== session?.expires_at) {
+				invalidate('supabase:auth')
+			}
+		})
+
+		return () => data.subscription.unsubscribe()
+	})
 </script>
 
 <!-- Navbar -->
@@ -26,5 +43,6 @@
   </li>
   <li><a>Item 3</a></li>
 </ul>
+
 
 <slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,16 @@
-<!-- localhost:5173 -->
-<div class="hero min-h-screen bg-base-200">
+<!-- src/routes/+page.svelte -->
+<script lang="ts">
+	import { Auth } from '@supabase/auth-ui-svelte'
+	import { ThemeSupa } from '@supabase/auth-ui-shared'
+
+	export let data
+</script>
+
+<!-- <svelte:head>
+	<title>User Management</title>
+</svelte:head> -->
+
+<!-- <div class="hero min-h-screen bg-base-200">
   <div class="hero-content text-center">
     <div class="max-w-md">
       <h1 class="text-5xl font-bold">Hello there</h1>
@@ -11,10 +22,20 @@
       <button class="btn btn-primary">Get Started</button>
     </div>
   </div>
+</div> -->
+
+<div class="row flex-center flex bg-base-200">
+	<div class="col-6 form-widget">
+		<Auth
+			supabaseClient={data.supabase}
+			view="magic_link"
+			redirectTo={`${data.url}/auth/callback`}
+			showLinks={false}
+		/>
+	</div>
 </div>
 
 <style lang="postcss">
-  :global(html) {
-    background-color: theme(colors.white);
-  }
 </style>
+
+
